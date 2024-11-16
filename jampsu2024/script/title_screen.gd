1a7f3d67aecfd64a3ab8e1d3ded9d8d3814d257a
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 19:03:09 +0100

    added stuff

--- jampsu2024/script/title_screen.gd
+++ jampsu2024/script/title_screen.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+func _ready() -> void:
+	var t := create_tween().set_trans(Tween.TRANS_CUBIC)
+	t.tween_property($CanvasLayer/ColorRect, "color", Color.TRANSPARENT, 0.4)
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
 		set_process(false)
 		get_tree().change_scene_to_file("res://scenes/world.tscn")
+		var t := create_tween().set_trans(Tween.TRANS_CUBIC)
+		t.tween_property($CanvasLayer/ColorRect, "color", Color.BLACK, 0.4)
