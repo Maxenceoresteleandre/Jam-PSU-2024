36344476e81deed4e95da56a363c326fd0aadca0
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 08:22:44 +0100

    anti bullshit class more successful

--- jampsu2024/script/collectible.gd
+++ jampsu2024/script/collectible.gd
@@ -1,2 +1,19 @@
 extends Sprite2D
 class_name Collectible
+
+const INTERACTIBLE_OBJECT_RES_PATH = preload("res://scenes/environment/interactible_objects/interactible_object_resource.tscn")
+
+var type : InteractibleResource.ResourceTypes
+var carried := false
+
+func deposit_object():
+	var obj_res : InteractibleObjectResource = INTERACTIBLE_OBJECT_RES_PATH.instantiate()
+	GlobalVariables.boat_view.add_child(obj_res)
+	obj_res.global_position = self.global_position
+	obj_res.set_obj_sprite(texture)
+	obj_res.resource = type
+	
+	queue_free()
+
+func carry_object():
+	carried = true
