2ce75e29c0eae3e0d35308c4e078109636ac187b
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 04:42:11 +0100

    noice

--- jampsu2024/script/global_variables.gd
+++ jampsu2024/script/global_variables.gd
@@ -1,1 +1,5 @@
 extends Node
+
+
+
+var small_boat : SmallBoat = null
--- jampsu2024/script/small_boat.gd
+++ jampsu2024/script/small_boat.gd
@@ -14,12 +14,13 @@ var speed: float = 70
 @export var current_speed:SPEEDS = SPEEDS.NORMAL
 @export var hit_obstacle_min_speed = 50
 @export var health: float = max_health
-@export var direction: Vector2 = Vector2(1,1)
+@export var direction: Vector2 = Vector2(1,0)
 
 @export var hit_cooldown_time = 2.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	GlobalVariables.small_boat = self
 	$health_debug.text = str(health) + "/" + str(max_health)
 	$speed_debug.text = "speed : " + str(speed)
 
@@ -32,16 +33,20 @@ func hit_obstacle():
 		can_move = true
 		$health_debug.text = str(health) + "/" + str(max_health)
 
+func set_turn(value : float):
+	direction = direction.rotated(value)
+	$Icon.rotation += value
+
 func damage(damage: float):
 	health -= damage
 	$Camera2D/CameraUtils.shake(0.3, 7, 20, 2)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
+func _process(_delta: float) -> void:
+	print("direction = ", direction)
 	pass
 	
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	$speed_debug.text = "speed : " + str(speed)
 	if can_move:
 		velocity = direction.normalized() * speed
