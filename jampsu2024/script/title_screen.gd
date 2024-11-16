ed2cb02cd10ee263c0e5aa9d4e2a61db661bed52
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 19:05:11 +0100

    ok???

--- jampsu2024/script/title_screen.gd
+++ jampsu2024/script/title_screen.gd
@@ -6,7 +6,4 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("interact"):
-		set_process(false)
 		get_tree().change_scene_to_file("res://scenes/world.tscn")
-		var t := create_tween().set_trans(Tween.TRANS_CUBIC)
-		t.tween_property($CanvasLayer/ColorRect, "color", Color.BLACK, 0.4)
