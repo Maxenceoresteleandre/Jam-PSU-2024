6411e209928b8e657fcfcd7f7bb6d3d663109659
Yopaman <pabloinscriptions@gmail.com>
2024-11-16 01:39:18 +0100

    boat move

--- jampsu2024/script/small_boat.gd
+++ jampsu2024/script/small_boat.gd
@@ -1,6 +1,7 @@
-extends Node2D
+extends CharacterBody2D
 
-
+@export var direction: Vector2 = Vector2(1,1)
+@export var speed: float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,3 +11,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	velocity = direction * speed
+	move_and_slide()
