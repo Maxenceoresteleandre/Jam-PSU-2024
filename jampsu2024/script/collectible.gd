19a02e5ee584c36993d303fe7ed61f13d3e577d2
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 09:50:48 +0100

    OK

--- jampsu2024/script/collectible.gd
+++ jampsu2024/script/collectible.gd
@@ -4,6 +4,7 @@ class_name Collectible
 
 var type : int
 var carried := false
+var can_be_carried_again := false
 
 func _ready() -> void:
 	carry_object()
@@ -15,6 +16,9 @@ func deposit_object():
 	GlobalVariables.boat_view.add_child(self)
 	self.global_position = gpos
 	global_scale = Vector2(5, 5)
+	can_be_carried_again = false
+	await get_tree().create_timer(0.2).timeout
+	can_be_carried_again = true
 
 func carry_object():
 	carried = true
@@ -23,7 +27,7 @@ func carry_object():
 	#global_scale = Vector2(5, 5)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if not carried and body is PlayerCharacter:
+	if not carried and body is PlayerCharacter and can_be_carried_again:
 		var pbody : PlayerCharacter = body
 		if not pbody.carrying_object:
 			get_parent().remove_child(self)
