d47b45a027d0b153270218a0a880bf85f8d9dc07
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 11:38:41 +0100

    created enemy paretn

--- jampsu2024/script/ennemy.gd
+++ jampsu2024/script/ennemy.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 class_name Ennemy
 
+
+@export var speed = 100
+@export var health := 1
 @export var aggro_range = 2000
+
 var chasing_player = false
 
 func _init() -> void:
@@ -13,4 +17,18 @@ func _physics_process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is SmallBoat:
 		chasing_player = true
-	
+
+func hit():
+	health -= 1
+	if health > 0:
+		hit_anim()
+	else:
+		death_anim()
+		await $AnimationPlayer.animation_finished
+		queue_free()
+
+func hit_anim():
+	$AnimationPlayer.play("hit_effect")
+
+func death_anim():
+	$AnimationPlayer.play("death_effect")
