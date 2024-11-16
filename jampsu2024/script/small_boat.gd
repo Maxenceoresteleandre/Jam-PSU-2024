eccbb8bc49f1b1f3d5a99c7ff7a5df5ecfa82fe5
Yopaman <pabloinscriptions@gmail.com>
2024-11-16 01:47:14 +0100

    voat view dans sea viw

--- jampsu2024/script/small_boat.gd
+++ jampsu2024/script/small_boat.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var direction: Vector2 = Vector2(1,1)
-@export var speed: float = 5.0
+@export var speed: float = 70.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
