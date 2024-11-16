cacd693ac6bbda2a1c11e62c7794943d29f6897d
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 14:43:29 +0100

    fg

--- jampsu2024/script/boat_view.gd
+++ jampsu2024/script/boat_view.gd
@@ -1,8 +1,26 @@
 extends Node2D
 class_name BoatView
 
+const GHOST_PATH := preload("res://scenes/environment/interactible_objects/ghost.tscn")
+
 @export var cannons : Array[Cannon] = []
+@export var min_time_ghost := 5.0
+@export var max_time_ghost := 18.0
+
+@onready var ghost_spawns := [$GhostSpawn.position, 
+$GhostSpawn2.position,
+$GhostSpawn3.position,
+$GhostSpawn4.position,
+]
+
 
 
 func _ready() -> void:
 	GlobalVariables.boat_view = self
+
+func spawn_ghost():
+	var ghost := GHOST_PATH.instantiate()
+	add_child(ghost)
+	ghost.position = ghost_spawns.pick_random()
+	await get_tree().create_timer(randf_range(min_time_ghost, max_time_ghost)).timeout
+	spawn_ghost()
