b3120019b820bda14c3af5c79e37b4472a071e78
Yopaman <pabloinscriptions@gmail.com>
2024-11-16 17:13:26 +0100

    score

--- jampsu2024/script/world.gd
+++ jampsu2024/script/world.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+var score = 0
+
 func _ready() -> void:
 	GlobalVariables.sea_view = $SeaView
+	GlobalVariables.world = self
+	
+func increment_score():
+	score += 1
+	$CanvasLayer/Label.text = str(score)
