dce140ef2386a2196b328e4284ee6d266e94d004
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-16 06:12:22 +0100

    wip cannon

--- jampsu2024/script/cannon.gd
+++ jampsu2024/script/cannon.gd
@@ -1,11 +1,43 @@
+@tool
 extends InteractibleObject
+class_name Cannon
 
+const CANNONBALL_RES := preload("res://scenes/environment/cannonball_boat.tscn")
+const CANNONBALL_OFFSET := 20.0
 
-# Called when the node enters the scene tree for the first time.
+@export var turn_speed := 1.0
+
+@export var left_side := true:
+	set(value):
+		left_side = value
+		$Sprite.flip_h = not left_side
+
+var sea_corresp : Node2D = null
+
 func _ready() -> void:
-	pass # Replace with function body.
+	create_sea_correspondance()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func create_sea_correspondance() -> void:
 	pass
+
+func interact() -> void:
+	var cannonball : Node2D = CANNONBALL_RES.instantiate()
+	self.add_child(cannonball)
+	var cannonball_velocity : Vector2
+	if left_side:
+		cannonball.position += Vector2.LEFT * CANNONBALL_OFFSET
+		cannonball_velocity = Vector2.LEFT
+	else:
+		cannonball.position += Vector2.RIGHT * CANNONBALL_OFFSET
+		cannonball_velocity = Vector2.RIGHT
+	cannonball.velocity = cannonball_velocity
+
+func _process(delta: float) -> void:
+	if current_player != null:
+		var turn_rate := 0.0
+		if current_player.up_pressed:
+			turn_rate += 1.0 
+		if current_player.down_pressed:
+			turn_rate -= 1.0
+		var rot_offset := turn_speed * delta * turn_rate
+		rotation += rot_offset
