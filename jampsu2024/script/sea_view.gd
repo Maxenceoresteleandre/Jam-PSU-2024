7e6f74561641a2357ac219b431a1879c00db7350
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-19 00:40:06 +0100

    wip chunks

--- jampsu2024/script/sea_view.gd
+++ jampsu2024/script/sea_view.gd
@@ -26,11 +26,29 @@ func _ready() -> void:
 		obj.connect("player_in_chunk", enter_chunk.bind(obj.chunk_pos))
 
 func enter_chunk(chunk_pos : Vector2i):
-	pass
+	current_chunk = chunk_pos
+	var new_chunks : Array[Vector2i] = []
+	for k : Vector2i in chunks.keys():
+		if (k.x < current_chunk.x - 1 or k.x > current_chunk.x + 1) or (
+			k.y < current_chunk.y - 1 or k.y > current_chunk.y + 1):
+			chunks[k].queue_free()
+			chunks.erase(k)
+		else:
+			new_chunks.append(k)
+	for x in range(current_chunk.x -1, current_chunk.x + 2):
+		for y in range(current_chunk.y -1, current_chunk.y + 2):
+			if not (Vector2i(x, y) in new_chunks):
+				var c : WorldChunk = spawn_chunk(Vector2i(x, y))
+				chunks[Vector2i(x, y)] = c
 
-func spawn_chunk():
+func spawn_chunk(chunk_coord : Vector2i) -> WorldChunk:
 	const CHUNK_PATH := preload("res://scenes/environment/world_chunk.tscn")
-	
+	var c := CHUNK_PATH.instantiate()
+	add_child(c)
+	c.position = chunk_coord * 2000.0
+	c.chunk_pos = chunk_coord
+	c.connect("player_in_chunk", enter_chunk.bind(c.chunk_pos))
+	return c
 
 func spawn_enemy():
 	const ENEMIES = [
