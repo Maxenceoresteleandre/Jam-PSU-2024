d86beeb336917d93a13b7e8021ab43e38b2fb913
Pablo <contact@pablo.town>
2024-11-18 22:00:56 +0100

    shader marche peut être ?

--- jampsu2024/script/water_trail.gd
+++ jampsu2024/script/water_trail.gd
@@ -1,15 +1,23 @@
 extends Line2D
 
 var queue: Array[Vector2] = []
+var length: float = 0.0
+@export var smallest_tip_width = 6
+@export var largest_tip_width = 15
+@export var distance_at_largest_width = 10
 
 func _process(delta: float) -> void:
 	var pos = get_parent().global_position
 	queue.push_front(pos)
 	
-	if (len(queue) > 70):
+	if (len(queue) > 120):
 		queue.pop_back()
 		
 	clear_points()
 	
-	for point in queue:
-		add_point(get_parent().to_local(point))
+	for i in range(len(queue) - 1):
+		length += queue[i].distance_to(queue[i + 1])
+		add_point(get_parent().to_local(queue[i]))
+
+	var width_value: float = lerp(smallest_tip_width, largest_tip_width, inverse_lerp(0, distance_at_largest_width, length))
+	width_curve.set_point_value(0, width_value)
