198eaad6fe625281ff85130446ccc8c39dfe8de9
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-18 16:00:56 +0100

    added random sprite on rocks

--- jampsu2024/script/obstacle.gd
+++ jampsu2024/script/obstacle.gd
@@ -1,7 +1,28 @@
 extends Area2D
 class_name Obstacle
 
+const SPRITES : Array = [
+	preload("res://scenes/environment/7.png"),
+	preload("res://scenes/environment/8.png"),
+	preload("res://scenes/environment/10.png"),
+	preload("res://scenes/environment/11.png"),
+	preload("res://scenes/environment/13.png"),
+	preload("res://scenes/environment/14.png"),
+	preload("res://scenes/environment/15.png"),
+	preload("res://scenes/environment/16.png"),
+	preload("res://resources/art/sea_view/Récifs/Deux récifs.png"),
+	preload("res://resources/art/sea_view/Récifs/Gros caillou.png"),
+	preload("res://resources/art/sea_view/Récifs/Gros caillou.png2.png"),
+	preload("res://resources/art/sea_view/Récifs/Récif droit.png"),
+	preload("res://resources/art/sea_view/Récifs/Récif gauche.png")
+	
+]
+
 @export var randomize_sprite := true
+
+func _ready() -> void:
+	if randomize_sprite:
+		$Icon.texture = SPRITES.pick_random()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is SmallBoat:
