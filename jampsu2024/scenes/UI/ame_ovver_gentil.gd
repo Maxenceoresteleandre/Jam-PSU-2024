a897b113a66d8538fabc73aea1787c0771a84ed1
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-18 15:18:02 +0100

    game over goes back to play

--- jampsu2024/scenes/UI/ame_ovver_gentil.gd
+++ jampsu2024/scenes/UI/ame_ovver_gentil.gd
@@ -2,3 +2,8 @@ extends CanvasLayer
 
 func _ready() -> void:
 	$TextScore.text = str(GameWorld.score)
+
+
+func _on_button_pressed() -> void:
+	GameWorld.score = 0
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
