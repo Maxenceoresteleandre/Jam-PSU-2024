91378b0e1e2165695746bbeaaaf315ad0c83f00f
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-18 15:38:44 +0100

    removed aggro range from enemy

--- jampsu2024/script/ennemy.gd
+++ jampsu2024/script/ennemy.gd
@@ -6,17 +6,7 @@ class_name Ennemy
 @export var health := 1
 @export var aggro_range = 2000
 
-var chasing_player = false
-
-func _init() -> void:
-	pass
-
-func _physics_process(delta: float) -> void:
-	pass
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is SmallBoat:
-		chasing_player = true
+var chasing_player = true
 
 func hit():
 	health -= 1
