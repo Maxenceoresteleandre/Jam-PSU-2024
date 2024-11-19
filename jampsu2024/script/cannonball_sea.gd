40a3a9514d63e7fd9e068138a83dc9092de4f145
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-19 01:16:42 +0100

    lowered ttl cannonball

--- jampsu2024/script/cannonball_sea.gd
+++ jampsu2024/script/cannonball_sea.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 const SPEED := 800.0
 
-var time_to_live := 500
+var time_to_live := 10.0
 var velocity : Vector2
 
 func _process(delta: float) -> void:
@@ -10,8 +10,6 @@ func _process(delta: float) -> void:
 	if time_to_live <= 0.0:
 		queue_free()
 	position += velocity * SPEED * delta
-
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if not (body is SmallBoat) and body.has_method("hit"):
