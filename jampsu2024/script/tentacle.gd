e90dc7f72d8773e4568f989ecca60df61aab7188
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-19 02:03:12 +0100

    add behavior to tentacles

--- jampsu2024/script/tentacle.gd
+++ jampsu2024/script/tentacle.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 class_name Tentacle
 
 const TIME_DOWN := 10.0
+const MIN_SPEED := 20.0
+const MAX_SPEED := 60.0
+
+var speed := 60.0
 
 @export var tentacle_texture : Texture = preload("res://resources/art/sea_view/kraken/tentacule 01.png"):
 	set(value):
@@ -10,14 +14,32 @@ const TIME_DOWN := 10.0
 		$Sprite2D.texture = tentacle_texture
 
 var is_up := true
+@onready var origin := position
+@onready var target = set_new_obj_point()
 
 func _ready() -> void:
 	await get_tree().create_timer(randf_range(0.0, 1.0)).timeout
 	$AnimatedSprite2D.play("default")
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Engine.is_editor_hint():
 		return
+	move_and_slide()
+	if position.distance_squared_to(target) < 100.0:
+		target = set_new_obj_point()
+
+func set_new_obj_point() -> Vector2:
+	speed = randf_range(MIN_SPEED, MAX_SPEED)
+	var v : Vector2 = origin + Vector2(
+		randf_range(-100, 100),
+		randf_range(-100, 100)
+	) 
+	velocity = position.direction_to(v) * speed
+	if velocity.x > 1:
+		$Sprite2D.flip_h = false
+	elif velocity.x < -1:
+		$Sprite2D.flip_h = true
+	return v
 
 func go_down():
 	is_up = false
