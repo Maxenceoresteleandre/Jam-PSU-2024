9eea36fa5aab3ecffd9df53b8ab647af41cdf3df
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-19 01:29:07 +0100

    wip kraken boss

---
+++ jampsu2024/script/kraken.gd
@@ -0,0 +1,6 @@
+extends Ennemy
+class_name Kraken
+
+
+func _on_hitbox_area_2d_body_entered(body: Node2D) -> void:
+	return
--- jampsu2024/script/tentacle.gd
+++ jampsu2024/script/tentacle.gd
@@ -1,24 +1,34 @@
+@tool
 extends CharacterBody2D
 class_name Tentacle
 
 const TIME_DOWN := 10.0
 
+@export var tentacle_texture : Texture = preload("res://resources/art/sea_view/kraken/tentacule 01.png"):
+	set(value):
+		tentacle_texture = value
+		$Sprite2D.texture = tentacle_texture
+
 var is_up := true
 
 func _process(delta: float) -> void:
-	pass
+	if Engine.is_editor_hint():
+		return
 
 func go_down():
+	is_up = false
+	$AnimationPlayer.play("hit")
+	await $AnimationPlayer.animation_finished
 	$Sprite2D.visible = false
 	$Timer.start(TIME_DOWN)
-	is_up = false
 
 func go_up():
 	$Sprite2D.visible = true
 	is_up = true
 
 func hit():
-	go_down()
+	if is_up:
+		go_down()
 
 func _on_hitbox_area_2d_body_entered(body: Node2D) -> void:
 	if body is SmallBoat and is_up:
