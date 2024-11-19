b9ecb6aa85e7d324ef2947ea5451b97d9936d47d
Maxence <78730104+Maxenceoresteleandre@users.noreply.github.com>
2024-11-19 02:42:00 +0100

    added script to tuto

--- jampsu2024/script/title_screen.gd
+++ jampsu2024/script/title_screen.gd
@@ -1,13 +1,41 @@
 extends Node2D
 
+var current_state := 0
+
 func _ready() -> void:
 	var t := create_tween().set_trans(Tween.TRANS_CUBIC)
 	t.tween_property($CanvasLayer/ColorRect, "color", Color.TRANSPARENT, 0.4)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("clicl"):
-		set_process(false)
-		var t := create_tween().set_trans(Tween.TRANS_CUBIC)
-		t.tween_property($CanvasLayer/ColorRect, "color", Color.BLACK, 0.4)
-		await get_tree().create_timer(0.5).timeout
-		get_tree().change_scene_to_file("res://scenes/world.tscn")
+		match current_state:
+			0:
+				var t := create_tween().set_trans(Tween.TRANS_CUBIC)
+				t.tween_property($CanvasLayer/ColorRect, "color", Color.BLACK, 0.4)
+				await t.finished
+				$CanvasLayer/TextureRect.visible = false
+				$CanvasLayer/TextureTuto.visible = true
+				t = create_tween().set_trans(Tween.TRANS_CUBIC)
+				t.tween_property($CanvasLayer/ColorRect, "color", Color.TRANSPARENT, 0.4)
+			1:
+				$CanvasLayer/TextureTuto/TutoLights.visible = true
+			2:
+				$CanvasLayer/TextureTuto/CanvasModulate.visible = true
+				$CanvasLayer/TextureTuto/TutoLights.visible = false
+				$CanvasLayer/TextureTuto/TutoCannons.visible = true
+			3:
+				$CanvasLayer/TextureTuto/TutoCannons.visible = false
+				$CanvasLayer/TextureTuto/TutoLightHouse.visible = true
+			4:
+				$CanvasLayer/TextureTuto/TutoLightHouse.visible = false
+				$CanvasLayer/TextureTuto/TutoWheel.visible = true
+			5:
+				$CanvasLayer/TextureTuto/CanvasModulate.visible = false
+				$CanvasLayer/TextureTuto/TutoWheel.visible = false
+			6:
+				set_process(false)
+				var t := create_tween().set_trans(Tween.TRANS_CUBIC)
+				t.tween_property($CanvasLayer/ColorRect, "color", Color.BLACK, 0.4)
+				await t.finished
+				get_tree().change_scene_to_file("res://scenes/world.tscn")
+		current_state += 1
